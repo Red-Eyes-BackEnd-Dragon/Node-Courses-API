7b36722153ab4c36432374db4fc0a9157bf80d5c
LukeMcCann <developer-lukemccann@outlook.com>
2021-07-08 22:22:14 +0100

    Basic Courses API

--- app.js
+++ app.js
@@ -3,9 +3,29 @@
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
+const courses = [
+    { id: 1, name: 'Software Engineering' },
+    { id: 2, name: 'Computer Science' },
+    { id: 3, name: 'Robotics' },
+    { id: 4, name: 'Artificial intelligence' },
+    { id: 5, name: 'Machine Learning' },
+    { id: 6, name: 'Theoretical Physics'}
+];
 
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    res.send(courses[req.params.id-1]);
+});
 
 app.listen(port, (req, res) => {
     console.log(`Listening on port ${port}`);
